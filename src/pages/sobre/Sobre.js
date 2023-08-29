e34aee0fd707312979ca5850883c6deb034d3f4f
Sérgio Koerich Pereira <129325112+sergiokoerich@users.noreply.github.com>
2023-08-28 22:13:40 -0300

    photo

--- src/pages/sobre/Sobre.js
+++ src/pages/sobre/Sobre.js
@@ -4,7 +4,7 @@ export default function Sobre(){
     return(
         <div className='h-screen bg-gray-950'>
           <div className='gap-5 slide-top animate-slide-top px-8 md:px-52 flex flex-col justify-center items-center md:flex-row md:justify-around md:py-44 py-32  '>
-            <img className="md:h-80 md:w-80 h-44 w-44 rounded-full  shadow-[0_0_30px_theme('colors.Blue')]" src={Eu} alt="Minha foto" />
+            <img className="md:h-80 md:w-80 h-28 w-28 rounded-full  shadow-[0_0_30px_theme('colors.Blue')]" src={Eu} alt="Minha foto" />
             <div className='mt-2 w-1/2 '>
                 <h3 className='mb-2 font-normal text-3xl text-center text-Blue'>Sobre mim</h3>
                 <h1 className='font-bold md:text-sm text-xs text-white md:mb-7 mb-0 text-center justify-center md:w-auto '>
